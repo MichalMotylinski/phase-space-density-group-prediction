f8f2dcb6163329b83e45a771c6130691a8bbe862
kapralm66 <48837250+MichalMotylinski@users.noreply.github.com>
2021-06-30 14:57:19 +0100

    Graph saving
    Added graph show and graph save option to the mixture best fit function.

--- graphs/mixture_fit.py
+++ graphs/mixture_fit.py
@@ -2,18 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import math
+import os
+import shutil
 
 
-def best_fit_mixture(model, data, host):
+def best_fit_mixture(model, data, host, fig_dir=None, show_graph=False, save_graph=False):
     """
     Draw best fit mixture.
 
     :param model: Gaussian Mixture model object
     :param data: Calculated densities
     :param host: Target star
+    :param fig_dir:
     :return: Plot a best fit mixture graph
     """
-    fig = plt.figure(figsize=(50, 5))
+    fig = plt.figure(figsize=(50, 5), facecolor="w")
     fig.subplots_adjust(left=0.12, right=0.97, bottom=0.21, top=0.9, wspace=0.5)
     ax = fig.add_subplot(131)
 
@@ -38,4 +41,9 @@ def best_fit_mixture(model, data, host):
     ax.set_xlabel('Phase space density')
     ax.set_ylabel('Probability density function')
     plt.legend()
-    plt.show()
+    fig.set_size_inches(25, 5)
+    if show_graph:
+        plt.show()
+    if save_graph:
+        plt.savefig(f"figures/{fig_dir}/{host[0]}", dpi=100, bbox_inches='tight', pad_inches=0.1)
+    plt.close()
