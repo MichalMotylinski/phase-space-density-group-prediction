e9ec41b65d6276387d19b54debf5813accfa8ed8
kapralm66 <48837250+MichalMotylinski@users.noreply.github.com>
2021-07-16 22:13:28 +0100

    Added combined gaussian mixture graph

--- graphs/mixture_fit.py
+++ graphs/mixture_fit.py
@@ -42,8 +42,54 @@ def best_fit_mixture(model, data, host, fig_dir=None, show_graph=False, save_gra
     ax.set_ylabel('Probability density function')
     plt.legend()
     fig.set_size_inches(25, 5)
-    if show_graph:
-        plt.show()
     if save_graph:
         plt.savefig(f"figures/{fig_dir}/{host[0]}", dpi=100, bbox_inches='tight', pad_inches=0.1)
+    if show_graph:
+        plt.show()
+    fig.clf()
+    plt.close()
+
+
+def combined_fit_mixture(model, data, host, fig_dir=None, show_graph=False, save_graph=False):
+    """
+    Draw best fit mixture.
+
+    :param model: Gaussian Mixture model object
+    :param data: Calculated densities
+    :param host: Target star
+    :param fig_dir:
+    :return: Plot a best fit mixture graph
+    """
+    fig = plt.figure(figsize=(50, 5), facecolor="w")
+    fig.subplots_adjust(left=0.12, right=0.97, bottom=0.21, top=0.9, wspace=0.5)
+    ax = fig.add_subplot(131)
+    colors = ["r", "b", "g", "k", "y"]
+    for i in range(5):
+        x = np.linspace(math.floor(data[i].min()), math.ceil(data[i].max()), data[i].shape[0])
+        logprob = model[i].score_samples(x.reshape(-1, 1))
+
+        responsibilities = model[i].predict_proba(x.reshape(-1, 1))
+        pdf = np.exp(logprob)
+
+        pdf_individual = responsibilities * pdf[:, np.newaxis]
+
+        plt.axvline(x=host[i][1], color=colors[i], label=f"{host[i][0]}: {host[i][1]}")
+
+        #ax.hist(data[i], 30, density=True, histtype='stepfilled', alpha=0.5)
+
+        # Add combined kde line
+        ax.plot(x, pdf, f'-{colors[i]}')
+        # Add individual lines for low and high density
+        ax.plot(x, pdf_individual, f'--{colors[i]}')
+    #ax.text(0.04, 0.96, "Best-fit Mixture", ha='left', va='top', transform=ax.transAxes)
+    plt.title("Best-fit Mixture")
+    ax.set_xlabel('Phase space density')
+    ax.set_ylabel('Probability density function')
+    plt.legend()
+    fig.set_size_inches(50, 8)
+    if save_graph:
+        plt.savefig(f"{fig_dir}/{host[0][0].rsplit('_', 1)[1]}", dpi=100, bbox_inches='tight', pad_inches=0.1)
+    if show_graph:
+        plt.show()
+    fig.clf()
     plt.close()
